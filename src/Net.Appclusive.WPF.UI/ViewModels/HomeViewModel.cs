a4f0b5bfdf7733a8ed8bdb0ad8ea2fd67b6e4b28
rufer7 <rufermarc@gmail.com>
2018-03-14 13:46:24 +0100

    Home.xaml
    * Textboxes added
    * Labels adjusted
    * Focus set
    
    #1

--- src/Net.Appclusive.WPF.UI/ViewModels/HomeViewModel.cs
+++ src/Net.Appclusive.WPF.UI/ViewModels/HomeViewModel.cs
@@ -1,18 +1,21 @@
 ﻿/**
- * Copyright 2018 d-fens GmbH
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Copyright 2018 d-fens GmbH
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+using Net.Appclusive.WPF.UI.Constants;
+using System.ComponentModel.DataAnnotations;
 
 namespace Net.Appclusive.WPF.UI.ViewModels
 {
@@ -20,7 +23,47 @@ namespace Net.Appclusive.WPF.UI.ViewModels
     {
         #region Properties
 
+        private string domain = CurrentApp.DefaultDomain;
 
+        [Required]
+        [MaxLength(Validation.MAX_LENGTH_32)]
+        public string Domain
+        {
+            get { return domain; }
+            set
+            {
+                domain = value;
+                RaisePropertyChangedEvent(nameof(Domain));
+            }
+        }
+
+        private string username;
+
+        [Required]
+        [MaxLength(Validation.MAX_LENGTH_32)]
+        public string Username
+        {
+            get { return username; }
+            set
+            {
+                username = value;
+                RaisePropertyChangedEvent(nameof(Username));
+            }
+        }
+
+        private string password;
+
+        [Required]
+        [MaxLength(Validation.MAX_LENGTH_32)]
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                RaisePropertyChangedEvent(nameof(Password));
+            }
+        }
 
         #endregion Properties
     }
